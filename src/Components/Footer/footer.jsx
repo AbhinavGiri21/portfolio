015abd4b59bd6f9b3585f01c727cd2a790753ea2
AbhinavGiri21 <abhinavgiri55@gmail.com>
2024-06-27 08:53:31 +0530

    update

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
         <div className="footer-container">
             <div className="social-link">
-                <a href="https://www.instagram.com/abhinav_giri21?igsh=aDJuaTJ0Z3g3ZTdh" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faInstagram} color="white" className='instagram' />
-                </a>
                 <a href="https://www.linkedin.com/in/abhinav-giri-644707256/" target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faLinkedin} color="white" className='linkedln' />
                 </a>
